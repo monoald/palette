91c7bebb029038fc9e9a488a5de48991a9b0f04c
Carlos S. Aldazosa <carlos.aldazosa@outlook.com>
2023-07-31 19:13:41 -0400

    [FEAT] sign in with google

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -40,12 +40,33 @@ export const SignIn = () => {
     navigate('/user/')
   }
 
+  const handleGoogleSignIn = () => {
+    const win: Window = window;
+    win.location = 'http://localhost:3000/api/v1/auth/google/callback'
+  }
+
   return (
     <SignLayer>
-      <Form fields={fields} submitEvent={submit} />
 
-      <div className='Sign__footer'>
-        <p>Don't have an account? <Link to='/signup'>Sign up</Link></p>
+      <div>
+        <Form fields={fields} submitEvent={submit} />
+
+        <div className='Sign__footer'>
+          <div className='Sign__options'>
+            <h2 className='Sign__subtitle'>
+              <span>Or Continue with</span>
+            </h2>
+            <button
+              className='google-sign border-hover-primary'
+              onClick={handleGoogleSignIn}
+            >
+              <span className='google-icon' />
+            </button>
+          </div>
+
+          <p>Don't have an account? <Link to='/signup'>Sign up</Link></p>
+        </div>
+
       </div>
     </SignLayer>
   )
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -30,15 +30,35 @@ const fileds: Field[] = [
 
 export const SignUp = () => {
   const submit = async (data: UserSignup) => {
-    const user = await signup(data)
+    await signup(data)
+  }
+
+  const handleGoogleSignIn = () => {
+    const win: Window = window;
+    win.location = 'http://localhost:3000/api/v1/auth/google/callback'
   }
 
   return (
     <SignLayer>
-      <Form fields={fileds} submitEvent={submit} />
 
-      <div className='Sign__footer'>
-        <p>Already on Palette? <Link to='/signin'>Sign in</Link></p>
+      <div>
+        <Form fields={fileds} submitEvent={submit} />
+
+        <div className='Sign__footer'>
+          <div className='Sign__options'>
+            <h2 className='Sign__subtitle'>
+              <span>Or Continue with</span>
+            </h2>
+            <button
+              className='google-sign border-hover-primary'
+              onClick={handleGoogleSignIn}
+            >
+              <span className='google-icon' />
+            </button>
+          </div>
+
+          <p>Already on Palette? <Link to='/signin'>Sign in</Link></p>
+        </div>
       </div>
     </SignLayer>
   )

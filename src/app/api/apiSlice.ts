25795e589aa93070639dba0acf9236d4a882ab32
Carlos S. Aldazosa <carlos.aldazosa@outlook.com>
2023-08-03 22:58:49 -0400

    [UPDATE] api url

--- src/app/api/apiSlice.ts
+++ src/app/api/apiSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { RootState } from '../store'
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'http://localhost:3000/api/v1',
+  baseUrl: 'https://palette.onrender.com/api/v1/',
   credentials: 'include',
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token

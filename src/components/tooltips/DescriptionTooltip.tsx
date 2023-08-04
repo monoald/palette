c2d3f201c7e2230c9f233cdbcb947116aebc118a
Carlos S. Aldazosa <carlos.aldazosa@outlook.com>
2023-08-03 23:01:17 -0400

    [FIX] for deploy

--- src/components/tooltips/DescriptionTooltip.tsx
+++ src/components/tooltips/DescriptionTooltip.tsx
@@ -9,7 +9,7 @@ interface DescriptionTooltipProps {
 
 export const DescriptionTooltip = ({ text, tipPosition }: DescriptionTooltipProps) => {
   return (
-    <div id='tool-tip' inert="true" role="tooltip" tip-position={tipPosition}>
+    <div id='tool-tip' role="tooltip" tip-position={tipPosition}>
       <span className='tool-tip__text'>{text}</span>
     </div>
   )

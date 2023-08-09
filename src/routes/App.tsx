8898fc63003bcce6435b5d15126cd6f9602f0322
Carlos S. Aldazosa <carlos.aldazosa@outlook.com>
2023-08-09 09:56:59 -0400

    [FIX] removed unused dependencies

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -14,7 +14,6 @@ import RequireAuth from '../features/auth/RequireAuth'
 
 import { UserRoutes } from './UserRoutes'
 import { authApiSlice } from '../features/auth/authApiSlice'
-import { GoogleOAuthProvider } from '@react-oauth/google'
 import Loader from '../pages/Loader'
 
 store.dispatch(authApiSlice.endpoints.getSaved.initiate())
@@ -22,30 +21,28 @@ store.dispatch(authApiSlice.endpoints.getSaved.initiate())
 export const App = () => {
   return (
     <Provider store={store}>
-      <GoogleOAuthProvider clientId='1086718373961-eerfusg737ja6bce29959c7p4bua3qka.apps.googleusercontent.com'>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<PaletteGenerator />} />
-            <Route path='/:palette' element={<PaletteGenerator />} />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<PaletteGenerator />} />
+          <Route path='/:palette' element={<PaletteGenerator />} />
 
-            <Route path='/signin'  element={<SignIn />} />
-            <Route path='/signup'  element={<SignUp />} />
+          <Route path='/signin'  element={<SignIn />} />
+          <Route path='/signup'  element={<SignUp />} />
 
-            <Route element={<RequireAuth />}>
-              <Route path='/user/*' element={<UserRoutes />} />
-            </Route>
+          <Route element={<RequireAuth />}>
+            <Route path='/user/*' element={<UserRoutes />} />
+          </Route>
 
-            <Route path='/color'>
-                <Route path='/color/:id' element={<Color />} />
-            </Route>
+          <Route path='/color'>
+              <Route path='/color/:id' element={<Color />} />
+          </Route>
 
-            <Route path='/palettes' element={<Palettes />} />
-            <Route path='/colors' element={<Colors />} />
+          <Route path='/palettes' element={<Palettes />} />
+          <Route path='/colors' element={<Colors />} />
 
-            <Route path='/loader' element={<Loader />} />
-          </Routes>
-        </BrowserRouter>
-      </GoogleOAuthProvider>
+          <Route path='/loader' element={<Loader />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   )
 }

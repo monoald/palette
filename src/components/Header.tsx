6845dce80817c8a11f96abd0dc840d02ef039ad4
Carlos S. Aldazosa <carlos.aldazosa@outlook.com>
2023-09-21 19:01:05 -0400

    [UPDATE] header navigator

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,59 +25,90 @@ export const Header = () => {
 
       <nav className='header-nav'>
         <ul className='header-nav__list'>
-          <li className='header-nav__item'>
-            <NavLink
-              to='/palettes'
-              className={({ isActive }) => 
-                isActive ? 'nav-link--active' : 'nav-link'
-              }
-            >
-              Palettes
-            </NavLink>
+          <li className='header-nav__item item-palettes'>
+            Palettes
+            <ul className='navigation'>
+              <li>
+                <NavLink
+                  to='/'
+                  className={({ isActive }) => 
+                    isActive ? 'nav-link--active' : 'nav-link'
+                  }
+                >
+                  Make palette
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink
+                  to='/palettes'
+                  className={({ isActive }) => 
+                    isActive ? 'nav-link--active' : 'nav-link'
+                  }
+                >
+                  Explore palettes
+                </NavLink>
+              </li>
+            </ul>
           </li>
 
-          <li className='header-nav__item'>
-            <NavLink
-              to='/colors'
-              className={({ isActive }) => 
-                isActive ? 'nav-link--active' : 'nav-link'
-              }
-            >
-              Colors
-            </NavLink>
+          <li className='header-nav__item item-colors'>
+            Colors
+            <ul className='navigation'>
+              <li>
+                <NavLink
+                  to='/colors'
+                  className={({ isActive }) => 
+                    isActive ? 'nav-link--active' : 'nav-link'
+                  }
+                >
+                  Explore colors
+                </NavLink>
+              </li>
+            </ul>
           </li>
 
-          <li className='header-nav__item'>
-            <NavLink
-              to='/'
-              className={({ isActive }) => 
-                isActive ? 'nav-link--active' : 'nav-link'
-              }
-            >
-              Palette Generator
-            </NavLink>
+          <li className='header-nav__item item-gradients'>
+            Gradients
+            <ul className='navigation'>
+              <li>
+                <NavLink
+                  to='/gradient'
+                  className={({ isActive }) => 
+                    isActive ? 'nav-link--active' : 'nav-link'
+                  }
+                >
+                  Make gradient
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink
+                  to='/gradients'
+                  className={({ isActive }) => 
+                    isActive ? 'nav-link--active' : 'nav-link'
+                  }
+                >
+                  Explore gradients
+                </NavLink>
+              </li>
+            </ul>
           </li>
 
-          <li className='header-nav__item'>
-            <NavLink
-              to='/gradients'
-              className={({ isActive }) => 
-                isActive ? 'nav-link--active' : 'nav-link'
-              }
-            >
-              Gradients
-            </NavLink>
-          </li>
-
-          <li className='header-nav__item'>
-            <NavLink
-              to='/create-icons-collection'
-              className={({ isActive }) => 
-                isActive ? 'nav-link--active' : 'nav-link'
-              }
-            >
-              Icon Font
-            </NavLink>
+          <li className='header-nav__item item-icons'>
+            Icon Fonts
+            <ul className='navigation'>
+              <li>
+                <NavLink
+                  to='/icons/create'
+                  className={({ isActive }) => 
+                    isActive ? 'nav-link--active' : 'nav-link'
+                  }
+                >
+                  Make icon fonts
+                </NavLink>
+              </li>
+            </ul>
           </li>
         </ul>
       </nav>

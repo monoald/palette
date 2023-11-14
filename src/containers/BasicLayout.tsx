73395ee23ed9dc7d39e03f68be5be9fda372b5ef
Carlos S. Aldazosa <carlos.aldazosa@outlook.com>
2023-11-13 21:31:37 -0400

    [UPDATE: styles] header

--- src/containers/BasicLayout.tsx
+++ src/containers/BasicLayout.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
 import { Header } from '../components/Header'
 import { Outlet } from 'react-router-dom'
+
+import '../styles/BasicLayout.css'
 
 const BasicLayout = () => {
   return (

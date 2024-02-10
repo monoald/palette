cdbfc3cbb8ff5d7fe06325707ecf70dd5b3a4873
monoald <carlos.aldazosa@outlook.com>
2024-02-10 18:51:09 -0400

    [UPDATE: message component] receive multiple messages and remove them in order

--- app/components/Message.tsx
+++ app/components/Message.tsx
@@ -1,59 +1,75 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import useStateHandler from "../(core)/hooks/useStateHandler";
+import { makeRandomID } from "../utils/makeRandomID";
 
 export default function Message() {
-  const [info, setInfo] = useState({
-    message: undefined,
-    type: undefined,
-  });
-
-  useEffect(() => {
-    if (info.message !== undefined) {
-      setTimeout(() => {
-        setInfo({
-          message: undefined,
-          type: undefined,
-        });
-      }, 5000);
-    }
-  });
+  const [info, setInfo] = useState<
+    {
+      message: string;
+      type: string;
+      id: string;
+    }[]
+  >([]);
 
   const updateMessageHandler = (e: Event) => {
     const event = e as CustomEvent;
 
-    setInfo({
-      message: event.detail.message,
-      type: event.detail.type,
-    });
+    setInfo([
+      ...info,
+      {
+        message: event.detail.message,
+        type: event.detail.type,
+        id: makeRandomID(),
+      },
+    ]);
+    setTimeout(() => {
+      setInfo((prev) => {
+        const newInfo = [...prev];
+        const messageIndex = newInfo.findIndex(
+          (msg) => msg.message === event.detail.message
+        );
+
+        if (messageIndex !== -1) {
+          newInfo.splice(messageIndex, 1);
+        }
+
+        return newInfo;
+      });
+    }, 5000);
   };
 
   useStateHandler([updateMessageHandler], ["custom:updateMessage"]);
 
-  const a = "".toUpperCase;
-
   return (
     <>
-      {info.message !== undefined && (
-        <div className="fixed bottom-8 left-8 text-base bg-[#03050C88] rounded-lg">
-          <div
-            className={`min-w-64 p-6 border border-secondary-border rounded-lg backdrop-blur-2xl flex items-center gap-4 text-[#fdfdfd]`}
-          >
-            {info.type === "general" && (
-              <span className="icon-info info-icon text-2xl" />
-            )}
-            {info.type === "success" && (
-              <span className="icon-check-round check-round-icon text-2xl" />
-            )}
-            {info.type === "warning" && (
-              <span className="icon-danger danger-icon text-2xl" />
-            )}
-            {info.type === "error" && (
-              <span className="icon-x-round x-round-icon text-2xl" />
-            )}
-            {info.message}
-          </div>
+      {info.length !== 0 && (
+        <div className="fixed bottom-8 left-8 w-fit h-fit">
+          {info.map((message) => (
+            <div
+              key={message.id}
+              className="absolute bottom-0 left-0 w-fit h-fit text-base bg-[#03050C88] rounded-lg"
+            >
+              <div
+                className={`min-w-64 w-fit p-6 border border-secondary-border rounded-lg backdrop-blur-2xl flex items-center gap-4 text-[#fdfdfd]`}
+              >
+                {message.type === "general" && (
+                  <span className="icon-info info-icon text-2xl" />
+                )}
+                {message.type === "success" && (
+                  <span className="icon-check-round check-round-icon text-2xl" />
+                )}
+                {message.type === "warning" && (
+                  <span className="icon-danger danger-icon text-2xl" />
+                )}
+                {message.type === "error" && (
+                  <span className="icon-x-round x-round-icon text-2xl" />
+                )}
+                <p className="w-full message-text">{message.message}</p>
+              </div>
+            </div>
+          ))}
         </div>
       )}
     </>

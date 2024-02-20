37e5d9b8c106c423606cf9077a5b28ec7be3e4a4
monoald <carlos.aldazosa@outlook.com>
2024-02-20 18:47:24 -0400

    [FEAT] footer

--- app/(core)/layout.tsx
+++ app/(core)/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} bg-main text-secondary`}>
         <User>
           <Header />
           {children}
--- app/(core)/page.tsx
+++ app/(core)/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { randomBetween } from "../utils/randomBetween";
 import { svgs } from "./font-icon/data/svg";
 import Image from "next/image";
+import Footer from "../components/Footer";
 
 const palettes = [
   {
@@ -153,127 +154,124 @@ export default function Home() {
   const icon = icons[randomBetween(0, icons.length - 1)];
 
   return (
-    <main className="w-full p-8 bg-main h-fit">
-      <div className="w-full max-w-6xl mx-auto flex flex-col gap-20">
-        <section className="flex flex-col gap-4">
-          <div
-            className="w-full mx-auto p-8 border border-secondary-border rounded-3xl flex flex-col gap-8"
-            style={{ background: palette.palette[palette.bg] }}
-          >
-            <p
-              className="text-6xl font-extrabold text-center text-balance"
-              style={{ color: palette.palette[palette.txt] }}
+    <>
+      <main className="w-full p-8 bg-main h-fit">
+        <div className="w-full max-w-6xl mx-auto flex flex-col gap-20">
+          <section className="flex flex-col gap-4">
+            <div
+              className="w-full mx-auto p-8 border border-secondary-border rounded-3xl flex flex-col gap-8"
+              style={{ background: palette.palette[palette.bg] }}
             >
-              Find the perfect palette for your project
-            </p>
+              <p
+                className="text-6xl font-extrabold text-center text-balance"
+                style={{ color: palette.palette[palette.txt] }}
+              >
+                Find the perfect palette for your project
+              </p>
 
-            <div className="w-fit mx-auto flex gap-4">
+              <div className="w-fit mx-auto flex gap-4">
+                <Link
+                  href="/palette/craft"
+                  className="rounded-lg w-32 h-10 font-semibold flex items-center justify-center"
+                  style={{
+                    background: palette.palette[palette["btn-1-bg"]],
+                    color: palette.palette[palette["btn-1-txt"]],
+                  }}
+                >
+                  start
+                </Link>{" "}
+                <Link
+                  href="/login"
+                  className="rounded-lg w-32 h-10 font-semibold flex items-center justify-center"
+                  style={{
+                    background: palette.palette[palette["btn-2-bg"]],
+                    color: palette.palette[palette["btn-2-txt"]],
+                  }}
+                >
+                  login
+                </Link>
+              </div>
+            </div>
+
+            <div className="w-full h-96 flex gap-2">
+              {palette.palette.map((clr) => (
+                <article
+                  key={clr}
+                  className="w-1/2 h-full border border-secondary-border rounded-3xl"
+                  style={{ background: clr }}
+                ></article>
+              ))}
+            </div>
+          </section>
+
+          <section className="flex flex-col gap-10">
+            <div className="flex flex-col items-center gap-8">
+              <p className="text-6xl font-extrabold text-center text-balance">
+                Discover amazing gradients
+              </p>
               <Link
-                href="/palette/craft"
-                className="rounded-lg w-32 h-10 font-semibold flex items-center justify-center"
-                style={{
-                  background: palette.palette[palette["btn-1-bg"]],
-                  color: palette.palette[palette["btn-1-txt"]],
-                }}
+                href="/gradient/craft"
+                className="relative w-fit py-3 px-10 text-secondary z-[0] before:absolute before:inset-1 before:rounded-3xl before:bg-main before:-z-[1] after:absolute after:rounded-3xl after:inset-0 after:[background-image:var(--ffrr)] after:-z-[2]"
+                style={{ "--ffrr": gradient } as React.CSSProperties}
               >
                 start
-              </Link>{" "}
-              <Link
-                href="/login"
-                className="rounded-lg w-32 h-10 font-semibold flex items-center justify-center"
-                style={{
-                  background: palette.palette[palette["btn-2-bg"]],
-                  color: palette.palette[palette["btn-2-txt"]],
-                }}
-              >
-                login
               </Link>
             </div>
-          </div>
 
-          <div className="w-full h-96 flex gap-2">
-            {palette.palette.map((clr) => (
-              <article
-                key={clr}
-                className="w-1/2 h-full border border-secondary-border rounded-3xl"
-                style={{ background: clr }}
-              ></article>
-            ))}
-          </div>
-        </section>
+            <div className="w-full h-fit border border-secondary-border rounded-3xl overflow-hidden">
+              <div
+                className="w-full h-[450px]"
+                style={{ background: gradient }}
+              ></div>
+            </div>
+          </section>
 
-        <section className="flex flex-col gap-10">
-          <div className="flex flex-col items-center gap-8">
+          <section className="flex flex-col gap-14">
             <p className="text-6xl font-extrabold text-center text-balance">
-              Discover amazing gradients
+              Make font icons for your apps
             </p>
-            <Link
-              href="/gradient/craft"
-              className="relative w-fit py-3 px-10 text-secondary z-[0] before:absolute before:inset-1 before:rounded-3xl before:bg-main before:-z-[1] after:absolute after:rounded-3xl after:inset-0 after:[background-image:var(--ffrr)] after:-z-[2]"
-              style={{ "--ffrr": gradient } as React.CSSProperties}
-            >
-              start
-            </Link>
-          </div>
 
-          <div className="w-full h-fit border border-secondary-border rounded-3xl overflow-hidden">
-            <div
-              className="w-full h-[450px]"
-              style={{ background: gradient }}
-            ></div>
-          </div>
-        </section>
-
-        <section className="flex flex-col gap-14">
-          <p className="text-6xl font-extrabold text-center text-balance">
-            Make font icons for your apps
-          </p>
-
-          <div className="flex flex-col items-center gap-4">
-            <Image
-              className="icon__svg"
-              width={36}
-              height={36}
-              src={`data:image/svg+xml;base64,${btoa(
-                unescape(encodeURIComponent(icon.content))
-              )}`}
-              alt={`icon ${icon.name}`}
-            />
-
-            <code className="w-fit p-8 bg-[#0D0D0D] rounded-3xl">
-              &lt;<span className="text-[#c73028]">span</span>{" "}
-              <span className="text-[#2E95D3]">class=</span>
-              <span className="text-[#f6af3c]">&quot;</span>
-              <span className="text-[#00A67D]">
-                icon-
-                {icon.name}
-              </span>
-              <span className="text-[#f6af3c]">&quot;</span> /&gt;
-            </code>
-          </div>
-
-          <div className="flex flex-wrap justify-center gap-12 mt-4">
-            {icons.map((svg) => (
+            <div className="flex flex-col items-center gap-4">
               <Image
-                key={svg.name}
                 className="icon__svg"
                 width={36}
                 height={36}
                 src={`data:image/svg+xml;base64,${btoa(
-                  unescape(encodeURIComponent(svg.content))
+                  unescape(encodeURIComponent(icon.content))
                 )}`}
-                alt={`icon ${svg.name}`}
+                alt={`icon ${icon.name}`}
               />
-            ))}
-          </div>
-        </section>
-      </div>
-      {/* <div className="flex items-center gap-2">
-        <span className="icon-logo text-5xl secondary-active"></span>
-        <h1 className="text-5xl secondary-active">
-          <Link href="/">Paleta</Link>
-        </h1>
-      </div> */}
-    </main>
+
+              <code className="w-fit p-8 bg-[#0D0D0D] rounded-3xl">
+                &lt;<span className="text-[#c73028]">span</span>{" "}
+                <span className="text-[#2E95D3]">class=</span>
+                <span className="text-[#f6af3c]">&quot;</span>
+                <span className="text-[#00A67D]">
+                  icon-
+                  {icon.name}
+                </span>
+                <span className="text-[#f6af3c]">&quot;</span> /&gt;
+              </code>
+            </div>
+
+            <div className="flex flex-wrap justify-center gap-12 mt-4">
+              {icons.map((svg) => (
+                <Image
+                  key={svg.name}
+                  className="icon__svg"
+                  width={36}
+                  height={36}
+                  src={`data:image/svg+xml;base64,${btoa(
+                    unescape(encodeURIComponent(svg.content))
+                  )}`}
+                  alt={`icon ${svg.name}`}
+                />
+              ))}
+            </div>
+          </section>
+        </div>
+      </main>
+      <Footer />
+    </>
   );
 }

32829e46be3edb797afc43f6e3acb2d944441f2a
Carlos S Aldazosa <carlos.aldazosa@outlook.com>
2024-05-17 23:39:31 -0400

    [CI/CD] fonticons service to vercel

--- packages/fonticons-service/src/index.js
+++ packages/fonticons-service/src/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
-const cors = require("cors");
 
-const { CLIENT_URI, PORT } = require("./config");
+const { PORT } = require("./config");
 const { boomErrorHandler, errorHandler } = require("./middlewares/error");
 
 const fonticonsRouter = require("./fonticons/router");
@@ -9,20 +8,6 @@ const fonticonsRouter = require("./fonticons/router");
 const app = express();
 
 app.set("port", PORT);
-
-const allowedOrigins = [CLIENT_URI];
-app.use(
-  cors({
-    credentials: true,
-    origin: (origin, callback) => {
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-  })
-);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -34,6 +19,4 @@ router.use("/", fonticonsRouter);
 app.use(boomErrorHandler);
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log("Server on port: " + PORT);
-});
+module.exports = { app };

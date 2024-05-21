318072e554c777efe541ed82bf8c5ba081c1475b
Carlos S Aldazosa <carlos.aldazosa@outlook.com>
2024-05-21 17:39:32 -0400

    [UPDATE] frontend ci/cd

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -9,6 +9,12 @@ const nextConfig = {
         port: "",
         pathname: "/*/**",
       },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/*/**",
+      },
     ],
   },
 };

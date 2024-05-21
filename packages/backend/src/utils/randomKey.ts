3ea3c354d486ca5e686c8873debd3e4aef7972ac
Carlos S Aldazosa <carlos.aldazosa@outlook.com>
2024-05-21 19:35:29 -0400

    [FIX] backend env variables

--- packages/backend/src/utils/randomKey.ts
+++ packages/backend/src/utils/randomKey.ts
@@ -1,7 +1,9 @@
 export function randomKey() {
-  const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789._-?@';
-  let key = '';
+  const charset =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789._-?@";
+  let key = "";
 
+  //
   for (let i = 0; i < 25; i++) {
     const randomIndex = Math.floor(Math.random() * charset.length);
     key += charset[randomIndex];
